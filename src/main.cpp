2f5bf1072883c826bbf96fb37d416fa0e2ca1a8a
Mustafa Ozturk <mozturkpro@gmail.com>
2022-06-07 16:40:11 -0400

    glsl output from vertex shader to input from fragment shader

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,19 @@ const char* WINDOW_TITLE = "Hello OpenGL";
 
 const char* vertexShader = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
+                                 "out vec4 vertexColor;\n"
                                  "void main()\n"
                                  "{\n"
                                  "  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "  vertexColor = vec4(0.5f, 0.0f, 0.0f, 1.0f);"
                                  "}\0";
 
 const char* fragmentShader = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
+                                   "in vec4 vertexColor;\n"
                                    "void main()\n"
                                    "{\n"
-                                   "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "    FragColor = vertexColor;\n"
                                    "}\0";
 
 int main()
@@ -53,6 +56,9 @@ int main()
         return -1;
     }
     std::cout << "openGL version: " << glGetString(GL_VERSION) << std::endl;
+    int maxVertexAttributes;
+    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &maxVertexAttributes);
+    std:: cout << "Maximum number of vertex attributes supported: " << maxVertexAttributes << std::endl;
 
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
     // update gl viewport when glfw window resizes
